8f3c7b9fac6315c70f97dad2280fb934a9cfcb30
Dave Davis <david@davertron.com>
2012-02-23 08:33:43 -0500

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :use_sudo, false
+
+set :application, "timetrack"
+set :repository,  "git://github.com/davertron/Time-Track.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "davertron.com"                          # Your HTTP server, Apache/etc
+role :app, "davertron.com"                          # This may be the same as your `Web` server
+role :db,  "davertron.com", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+set :deploy_to, "/var/www/rails"
+set :user, "deploy"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :start do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
