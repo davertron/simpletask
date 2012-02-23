86e876905c8c3d300e03f6067c743104cea564a1
Dave Davis <david@davertron.com>
2012-02-23 08:33:57 -0500

    Don't allow duplicate usernames.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User
 
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+  validates_uniqueness_of :username
 
   has_many :tasks
 
