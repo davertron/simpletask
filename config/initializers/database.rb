40a6fe2bec0efdac546707421c70499ab5501bbc
David Davis <david.davis@dealer.com>
2013-01-09 13:58:21 -0500

    Fix mongo connection errors in passenger
    * Remove code from database.yml since we're not using activerecord.
    * Add initializer to handle connecting to mongo when starting up in
    passenger

---
+++ config/initializers/database.rb
@@ -0,0 +1,14 @@
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+if ['development', 'test'].include? Rails.env 
+    MongoMapper.database = "timetrack-#{Rails.env}"
+else
+    MongoMapper.database = 'timetrack'
+end
+
+
+if defined?(PhusionPassenger)
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+        MongoMapper.connection.connect if forked
+    end
+end
+
